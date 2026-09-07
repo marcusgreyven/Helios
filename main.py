470e6ab5f5ae4a9f6cf072a59df7ed1e804fd4a6
Mark <seredamark009@gmail.com>
2026-09-08 00:35:59 +0300

    Update Helios files v3

--- main.py
+++ main.py
@@ -65,10 +65,9 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "main:app",
+        app,
         host="0.0.0.0",
-        port=8000,
-        reload=False
+        port=8000
     )
 
 @app.post("/api/camera/capture")
